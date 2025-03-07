2410f04b6667565c70b967540e96f6dfc7bfc919
Edmundo Abreu e Lima <engedmundo@gmail.com>
2025-03-06 21:56:49 -0300

    exe

---
+++ aula_3/exercicio_3-5.py
@@ -0,0 +1,34 @@
+from random import choice
+
+def comparar_jogadas(usuario, computador):
+    if usuario == computador:
+        print("Empate")
+    else:
+        if usuario == "pedra" and computador == "papel":
+            print("Computador ganhou!!!")
+        elif usuario == "pedra" and computador == "tesoura":
+            print("Usuário ganhou!!!")
+        elif usuario == "papel" and computador == "pedra":
+            print("Usuário ganhou!!!")
+        elif usuario == "papel" and computador == "tesoura":
+            print("Computador ganhou!!!")
+        elif usuario == "tesoura" and computador == "pedra":
+            print("Computador ganhou!!!")
+        elif usuario == "tesoura" and computador == "papel":
+            print("Usuário ganhou!!!")
+
+
+
+
+
+jogadas_possiveis = ["pedra", "papel", "tesoura"]
+
+# ler jogada usuário
+jogada_usuario = input("Digite sua jogada: ")
+jogada_usuario = jogada_usuario.lower()
+
+# gerar jogada computador
+jogada_computador = choice(jogadas_possiveis)
+
+# comparar jogadas
+comparar_jogadas(jogada_usuario, jogada_computador)
