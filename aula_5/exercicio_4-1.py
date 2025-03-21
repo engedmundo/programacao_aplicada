cda4920b9d9c2838943a74bf3ee187c84a036a32
Edmundo Abreu e Lima <engedmundo@gmail.com>
2025-03-20 21:33:47 -0300

    exe

--- aula_5/exercicio_4-1.py
+++ aula_5/exercicio_4-1.py
@@ -1,7 +1,9 @@
 """
 Crie um programa para gerar duas matrizes quadradas aleatórias (A e B),
 no intervalo [1, 10], de mesma ordem, a qual deve ser informada pelo
-usuário. Ao fim, o programa deve calcular e imprimir a soma entre os
+usuário. 
+
+Ao fim, o programa deve calcular e imprimir a soma entre os
 elementos de A que estão abaixo da diagonal principal com os elementos
 de B que estão acima da diagonal principal.
 """
@@ -24,10 +26,44 @@ def gerar_matriz_quadrada(ordem):
     return matriz
 
 
+def imprime_matriz(matriz):
+    for linha in matriz:
+        print(linha)
+
+
+def soma_acima_dp(matriz):
+    soma = 0
+    for idx_linha, linha in enumerate(matriz):
+        for idx_coluna, valor in enumerate(linha):
+            if idx_coluna >= idx_linha:
+                soma = soma + valor
+
+    return soma
+
+def soma_abaixo_dp(matriz):
+    soma = 0
+    for idx_linha, linha in enumerate(matriz):
+        for idx_coluna, valor in enumerate(linha):
+            if idx_coluna <= idx_linha:
+                soma = soma + valor
+
+    return soma
+
+
 ordem = int(input("Digite a ordem das matrizes: "))
 
 matriz_A = gerar_matriz_quadrada(ordem)
 matriz_B = gerar_matriz_quadrada(ordem)
 
-print(matriz_A)
-print(matriz_B)
+print("Matriz A:")
+imprime_matriz(matriz_A)
+print("Matriz B:")
+imprime_matriz(matriz_B)
+
+soma_elementos_A = soma_abaixo_dp(matriz_A)
+soma_elementos_B = soma_acima_dp(matriz_B)
+
+print(f"Soma elementos matriz A: {soma_elementos_A}")
+print(f"Soma elementos matriz B: {soma_elementos_B}")
+soma_final = soma_elementos_A + soma_elementos_B
+print(f"Soma final: {soma_final}")
